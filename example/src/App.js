8eabd0cb0022c34f36f4acdc0f8d375706daf78d
Dallon Feldner <dfeldner@phunware.com>
2017-12-11 14:42:15 -0600

    Example app

--- example/src/App.js
+++ example/src/App.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import StableUniqueId, { withStableUniqueId } from 'react-stable-uniqueid';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h4>Simple usage:</h4>
+        <StableUniqueId
+          render={({ uniqueId }) => <div>Unique ID: {uniqueId}</div>}
+        />
+        <h4>Stable across re-renders:</h4>
+        <ExampleWithState />
+        <h4>As a higher-order component:</h4>
+        <ExampleWithHOC customProp="Hello world!" />
       </div>
     );
   }
 }
+
+class ExampleWithState extends Component {
+  state = { counter: 0 };
+
+  render() {
+    return (
+      <StableUniqueId
+        render={({ uniqueId }) => (
+          <div>
+            Unique ID: {uniqueId}
+            <br />Counter: {this.state.counter}
+            <br />
+            <button
+              onClick={() =>
+                this.setState(prevState => ({
+                  counter: prevState.counter + 1,
+                }))
+              }
+            >
+              Increment counter
+            </button>
+          </div>
+        )}
+      />
+    );
+  }
+}
+
+const ExampleWithHOC = withStableUniqueId()(({ customProp, uniqueId }) => (
+  <div>
+    Unique ID: {uniqueId}
+    <br />Custom Prop: {customProp}
+  </div>
+));
 
 export default App;
